0d80afb4ca0f07dcad9da1291310e38e50d99346
jaqi <jaq.cat@protonmail.ch>
2021-09-19 09:49:59 -0300

    very very cool

--- application/forms.py
+++ application/forms.py
@@ -3,11 +3,12 @@ from wtforms import PasswordField
 from wtforms.validators import DataRequired, Length
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.fields.simple import TextField
+from wtforms.widgets.core import TextArea
 
 class LoginForm(FlaskForm):
     password = PasswordField(validators=[DataRequired()])
 
 class PostForm(FlaskForm):
     title = TextField('Title', validators=[DataRequired(), Length(max=120)])
-    body = TextField('Body', validators=[DataRequired(), Length(max=1000)])
+    body = TextField('Body', validators=[DataRequired(), Length(max=1000)], widget=TextArea())
     # image = FileField('image', validators=[FileRequired(), FileAllowed(['jpg', 'jpeg', 'png', 'webp', 'bmp'], 'Images only')])
--- application/models.py
+++ application/models.py
@@ -1,16 +1,21 @@
 # type: ignore
 from application import db
+from datetime import datetime as dt
 from uuid import uuid4
 import os
 
+def _today():
+    td = dt.today()
+    return dt(td.year, td.month, td.day, td.hour, td.minute)
+
 class Post(db.Model):
     __tablename__ = 'posts'
-    uid = db.Column(db.String(36), primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(1000))
+    uid = db.Column(db.String(36), primary_key=True, nullable=False, default=str(uuid4()))
+    title = db.Column(db.String(120), nullable=False)
+    post_time = db.Column(db.DateTime, nullable=False, default=_today())
+    body = db.Column(db.String(1000), nullable=False)
 
     def __init__(self, title: str, body: str):
-        self.uid = str(uuid4())
         self.title = title
         self.body = body
 
